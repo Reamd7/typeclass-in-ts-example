543cdb98cf12b1c33141410acf0c0b1ca1057f51
Reamd7 <22834225+Reamd7@users.noreply.github.com>
2023-04-13 11:18:27 +0800

    2 files modified, 2 added

---
+++ index.ts
@@ -0,0 +1,1 @@
+import './src/index_hkt.ts';
--- src/xstream_display_feature.ts
+++ src/xstream_display_feature.ts
@@ -1,25 +1,24 @@
-import { $ } from "./typeclasses/index";
-import { Display } from "./display_feature";
-import { XStream } from "./xstream_adt";
+import { $ } from './typeclasses/index';
+import { Display } from './display_feature';
+import { XStream } from './xstream_adt';
 
-export class XStreamDisplay implements Display<"XStream"> {
-    print<A>(fa: $<"XStream", A>): string {
-      console.log(fa.value)
-      return String(fa.value)
-    }
+export class XStreamDisplay implements Display<'XStream'> {
+  print<A>(fa: $<'XStream', A>): string {
+    console.log('console display', fa.value);
+    return String(fa.value);
+  }
 }
 
-
 declare module '@hkt' {
-    interface _<A> {
-      "XStream": XStream<A>
-    }
+  interface _<A> {
+    XStream: XStream<A>;
+  }
 }
 
 declare module './display_feature' {
-    namespace Display {
-      export let XStream: XStreamDisplay
-    }
+  namespace Display {
+    export let XStream: XStreamDisplay;
+  }
 }
 
 Display.XStream = new XStreamDisplay();
